376773ca0b9132af7d1e8dffe29fb69d87507915
Adel_Yogurt <1415071309@qq.com>
2024-07-26 20:25:50 +0800

    'temp'

--- src_solver/preModelPanel.m
+++ src_solver/preModelPanel.m
@@ -1,32 +1,18 @@
-function preModelPanel(config)
+function user_model=preModelPanel(config)
 % prepare model for panel method
 % calculate geometry properties of element
 %
-% abbreviation:
-% elem: element, num: number, idx: index
-%
 % copyright Adel 2023.03
 %
-
-global user_model
-
 config=config.data_dict;
 user_model.config=config;
 
 % initialize model
-user_model.geometry=[];
-user_model.vertex_list=[];
-user_model.element_list=[];
+user_model.point_list=[];
 user_model.marker_list=[];
+user_model.element_list=[];
 
-user_model.output_inviscid=[];
-user_model.output_streamline=[];
-user_model.output_boulay=[];
-user_model.output_viscid=[];
-user_model.output_heat=[];
-user_model.output_post=[];
-
-% load mesh data
+%% load mesh data
 if ~isfield(config,'mesh_data') || isempty(config.mesh_data)
     switch config.MESH_FORMAT
         case 'SU2'
@@ -38,13 +24,13 @@ if ~isfield(config,'mesh_data') || isempty(config.mesh_data)
                 mesh_filestr_list={mesh_filestr_list};
             end
             mesh_data=struct();
-            marker_moniter=cell(length(mesh_filestr_list),1);
+            mkr_morit=cell(length(mesh_filestr_list),1);
 
             for file_index=1:length(mesh_filestr_list)
                 mesh_filestr=mesh_filestr_list{file_index};
                 mesh_data_new=readMeshSTL(mesh_filestr,config.MESH_SCALE);
                 mkr_name=fieldnames(mesh_data_new);mkr_name=mkr_name{1};
-                marker_moniter{file_index}=mkr_name;
+                mkr_morit{file_index}=mkr_name;
                 mesh_data.(mkr_name)=mesh_data_new.(mkr_name);
             end
 
@@ -53,7 +39,7 @@ if ~isfield(config,'mesh_data') || isempty(config.mesh_data)
 
             % for STL file, if MARKER_MONITORING than will analysis all file
             if ~isfield(config,'MARKER_MONITORING')
-                config.MARKER_MONITORING=marker_moniter;
+                config.MARKER_MONITORING=mkr_morit;
             end
         case 'INP'
             % read SU2 format mesh data into mesh format
@@ -80,24 +66,20 @@ end
 
 % process marker monitoring
 if ~isfield(config,'MARKER_MONITORING')
-    config.MARKER_MONITORING=[];
+    error('preModelPanel: lack marker monitered');
 else
-    if strcmp(config.MARKER_MONITORING,'NONE')
-        config.MARKER_MONITORING=[];
-    end
-
     if ischar(config.MARKER_MONITORING)
         config.MARKER_MONITORING={config.MARKER_MONITORING};
     end
 
     % check MARKER_MONITORING if exist in mesh data
-    for marker_moniter_idx=1:length(config.MARKER_MONITORING)
-        marker_moniter=config.MARKER_MONITORING{marker_moniter_idx};
+    for mkr_morit_idx=1:length(config.MARKER_MONITORING)
+        mkr_morit=config.MARKER_MONITORING{mkr_morit_idx};
         exist_flag=0;
-        mesh_marker=fieldnames(mesh_data);
-        for mkr_idx=1:length(mesh_marker)
-            if strcmp(mesh_marker{mkr_idx},marker_moniter) &&...
-                    ~strcmp(mesh_marker{mkr_idx},'geometry')
+        msh_mkr_list=fieldnames(mesh_data);
+        for mkr_idx=1:length(msh_mkr_list)
+            if strcmp(msh_mkr_list{mkr_idx},mkr_morit) &&...
+                    ~strcmp(msh_mkr_list{mkr_idx},'geometry')
                 exist_flag=1;
                 break;
             end
@@ -109,11 +91,65 @@ else
     end
 end
 
-% convert mesh_data to vertex_list
-[element_list,marker_list]=preModelMesh(mesh_data,config.MARKER_MONITORING);
-geometry=mesh_data.geometry;
+%% convert mesh_data to dimension, point_list, element_list and marker_list
 
-point_list=geometry.point_list;
+dim=mesh_data.geometry.dimension;
+pnt_list=mesh_data.geometry.point_list;
+
+mkr_morit_list=config.MARKER_MONITORING;
+mkr_num=length(mkr_morit_list);
+
+% obtain element number of all monitoring maker
+elem_num=0;
+for mkr_idx=1:mkr_num
+    mkr_name=mkr_morit_list{mkr_idx};
+    mkr=mesh_data.(mkr_name);
+
+    if mkr.ID == 20 % mixed element
+        elem_num=elem_num+length(mkr.number_list);
+    else
+        elem_num=elem_num+size(mkr.element_list,1);
+    end
+end
+
+% allocate memory 
+if dim == 2, elem_list=nan(elem_num,2);
+else, elem_list=nan(elem_num,4);end
+
+% generate element_list and marker_list
+elem_idx=1;
+for mkr_idx=1:mkr_num
+    mkr_name=mkr_morit_list{mkr_idx};
+    mkr=mesh_data.(mkr_name);
+    ID=mkr.ID;
+    mkr_elem_list=mkr.element_list;
+    mkr_num_list=mkr.number_list;
+
+    % convert each mesh into Vertex
+    if ID == 20 % mixed element
+        mkr_elem_num=length(mkr.number_list);
+        mkr_elem_idx=uint32((elem_idx):(elem_idx-1+mkr_elem_num))';
+
+        node_idx=1;
+        for idx=1:mkr_elem_num
+            node_num=mkr_num_list(idx);
+            elem_list(elem_idx,1:node_num)=mkr_elem_list(node_idx+(1:node_num));
+            elem_idx=elem_idx+1;
+            node_idx=node_idx+node_num+1;
+        end
+    else
+        mkr_elem_num=size(mkr.element_list,1);
+        mkr_elem_idx=uint32((elem_idx):(elem_idx-1+mkr_elem_num))';
+
+        elem_list(mkr_elem_idx,1:mkr_num_list)=mkr_elem_list;
+        elem_idx=elem_idx+mkr_elem_num;
+    end
+
+    % index list for element of marker
+    mkr_list.(mkr_name)=mkr_elem_idx;
+end
+
+%% 
 
 point_num=size(point_list,1);
 elem_num=length(element_list);
@@ -196,6 +232,8 @@ for elem_base_idx=1:elem_num
     end
 end
 
+%% calculate basical geometry data of node
+
 elem_num=length(element_list);
 % calculate center_point_list, normal_vector_list and area_list
 center_point_list=zeros(elem_num,3);
@@ -242,5 +280,5 @@ geometry.normal_vector_list=normal_vector_list;
 user_model.geometry=geometry;
 user_model.vertex_list=vertex_list;
 user_model.element_list=element_list;
-user_model.marker_list=marker_list;
+user_model.marker_list=mkr_list;
 end
